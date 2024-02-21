c263625dd8a6aad973c32b92596dc71a7246f8ce
ArnaudBand <bandonkeyea@gmail.com>
2024-02-21 22:44:41 +0300

    Add dynamic search

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import './App.css';
 import NewsCard from "./components/NewsCard";
 
 function App() {
+  
+  const options = { year: 'numeric',  month: '2-digit', day: '2-digit' };
+  const today = new Date().toLocaleDateString("en-GB", options);
+  
   const [news, setNews] = useState([]);
-  const [searchQuery, setSearchQuery] = useState("react");
+  const [searchQuery, setSearchQuery] = useState("");
   const [ isLoading, setIsLoading ] = useState (false);
-
-    const options = { year: 'numeric',  month: '2-digit', day: '2-digit' };
-    const today = new Date().toLocaleDateString("en-GB", options);
+  const [ url, setUrl ] = useState (`https://newsapi.org/v2/everything?q=Apple&from=${ today }&sortBy=popularity&apiKey=${ process.env.REACT_APP_API_KEY }`)
+  
 
     const fetchNews = () => {
       setIsLoading(true);
-        fetch(`https://newsapi.org/v2/everything?q=${searchQuery}&from=${today}&sortBy=popularity&apiKey=${process.env.REACT_APP_API_KEY}`)
+        fetch(url)
             .then((res) => res.json())
             .then((data) => {
               setNews(data.articles);
@@ -30,17 +33,19 @@ function App() {
 
   useEffect(() => {
     fetchNews()
-  }, [searchQuery]);
+  }, [url]);
     
     const handleSubmit = (e) => {
       e.preventDefault();
+      setUrl(`https://newsapi.org/v2/everything?q=${ searchQuery }&from=${ today }&sortBy=popularity&apiKey=${ process.env.REACT_APP_API_KEY }`)
+      setSearchQuery("");
     }
 
   return (
       <div className={"flex flex-col gap-y-4 mx-3"}>
         <h3 className="text-xl font-bold text-center">Actuality</h3>
-        <form className={'flex items-center justify-center rounded bg-gray-500 p-3 w-fit gap-x-2'}>
-          <input type={"text"} value={searchQuery} onChange={handleChange} className={'border-none rounded pl-3'} />
+        <form onSubmit={handleSubmit} className={'flex items-center justify-center rounded bg-gray-500 p-3 w-fit gap-x-2'}>
+          <input type={"text"} value={searchQuery} onChange={handleChange} className={'border-none rounded pl-3 bg-black text-white'} />
           <button className={'bg-black text-white px-2 rounded'}>Search</button>
         </form>
         {isLoading ? (
