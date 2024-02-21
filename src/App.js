aa265855bf02d41aa9223dbef1ad320a7e2f6eba
ArnaudBand <bandonkeyea@gmail.com>
2024-02-21 11:16:34 +0300

    Add search functionality

--- src/App.js
+++ src/App.js
@@ -4,11 +4,16 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [news, setNews] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("International")
 
   const fetchNews = () => {
-    fetch(`https://api.worldnewsapi.com/search-news?api-key=${process.env.REACT_APP_API_KEY}&text="International"`)
+    fetch(`https://api.worldnewsapi.com/search-news?api-key=${process.env.REACT_APP_API_KEY}&text=${searchQuery}`)
         .then((res) => res.json())
         .then((data) => (setNews(data.news), console.log(data.news))) // Assuming the data structure has an 'articles' field
+  }
+
+  const handleChange = (e) => {
+    setSearchQuery(e.target.value);
   }
 
   useEffect(() => {
@@ -18,6 +23,10 @@ function App() {
   return (
       <div className={"flex flex-col gap-y-4 mx-3"}>
         <h3 className="text-xl font-bold text-center">Actuality</h3>
+        <form className={'flex items-center justify-center rounded bg-gray-500 py-3'}>
+          <input type={"text"} value={searchQuery} onChange={handleChange} />
+          <button>Search</button>
+        </form>
         {news.map((actuality) => (
             <div className={"rounded bg-gray-100 p-4"} key={actuality.id}>
               <h3 className={"text-lg font-bold mb-2"}>{actuality.title}</h3>
