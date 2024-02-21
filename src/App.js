cb90725880b424e3059ec56c72b87ff117d5a03b
ArnaudBand <bandonkeyea@gmail.com>
2024-02-21 17:13:50 +0300

    Refactor the code

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import './App.css';
 import { useEffect, useState } from "react";
-
+import './App.css';
+import NewsCard from "./components/NewsCard";
 
 function App() {
   const [news, setNews] = useState([]);
-  const [searchQuery, setSearchQuery] = useState("International")
+  const [searchQuery, setSearchQuery] = useState("react");
+  const [ isLoading, setIsLoading ] = useState (false);
 
-  const fetchNews = () => {
-    fetch(`https://api.worldnewsapi.com/search-news?api-key=${process.env.REACT_APP_API_KEY}&text=${searchQuery}`)
-        .then((res) => res.json())
-        .then((data) => (setNews(data.news), console.log(data.news))) // Assuming the data structure has an 'articles' field
-  }
+    const options = { year: 'numeric',  month: '2-digit', day: '2-digit' };
+    const today = new Date().toLocaleDateString("en-GB", options);
+
+    const fetchNews = () => {
+      setIsLoading(true);
+        fetch(`https://newsapi.org/v2/everything?q=${searchQuery}&from=${today}&sortBy=popularity&apiKey=${process.env.REACT_APP_API_KEY}`)
+            .then((res) => res.json())
+            .then((data) => {
+              setNews(data.articles);
+              setIsLoading(false);
+            })
+            .catch((error) => {
+              console.log ( error.message );
+              setIsLoading(false);
+            })
+    }
 
   const handleChange = (e) => {
     setSearchQuery(e.target.value);
@@ -18,22 +30,28 @@ function App() {
 
   useEffect(() => {
     fetchNews()
-  }, []);
+  }, [searchQuery]);
+    
+    const handleSubmit = (e) => {
+      e.preventDefault();
+    }
 
   return (
       <div className={"flex flex-col gap-y-4 mx-3"}>
         <h3 className="text-xl font-bold text-center">Actuality</h3>
-        <form className={'flex items-center justify-center rounded bg-gray-500 py-3'}>
-          <input type={"text"} value={searchQuery} onChange={handleChange} />
-          <button>Search</button>
+        <form className={'flex items-center justify-center rounded bg-gray-500 p-3 w-fit gap-x-2'}>
+          <input type={"text"} value={searchQuery} onChange={handleChange} className={'border-none rounded pl-3'} />
+          <button className={'bg-black text-white px-2 rounded'}>Search</button>
         </form>
-        {news.map((actuality) => (
-            <div className={"rounded bg-gray-100 p-4"} key={actuality.id}>
-              <h3 className={"text-lg font-bold mb-2"}>{actuality.title}</h3>
-              <img src={actuality.image ? actuality.image : ''} alt={'news image'} className="mx-auto"/>
-              <p className="text-gray-700 mt-2">{actuality.text.substring(0, 100)}...</p>
-            </div>
-        ))}
+        {isLoading ? (
+        <p className="text-center">Loading...</p>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            {news.map((article) => (
+              <NewsCard article={article} />
+            ))}
+          </div>
+        )}
       </div>
   );
 }
---
+++ src/components/NewsCard.jsx
@@ -0,0 +1,15 @@
+const NewsCard = ({ article }) => (
+  <div className={ "rounded bg-gray-100 p-4 cursor-pointer h-fit" } key={ article.id }>
+    <h3 className={ "text-lg font-bold mb-2" }>{ article.title }</h3>
+    <img
+      src={ article.urlToImage === null ? 'https://kubrick.htvapps.com/vidthumb/f6865cb1-d77d-4a31-ba83-d57c4b2324d8/4b9c9d8f-ad14-47ea-bcf4-bf24ee0bb1f3.jpg?crop=0.383xw:0.383xh;0.517xw,0.252xh&resize=1200:*' : article.urlToImage }
+      alt={ 'news image' } className="mx-auto"/>
+    <p className="text-gray-700 my-2">{ article.description.substring ( 0, 100 ) }...</p>
+    <div className={ 'flex justify-between' }>
+      <p className={ 'bg-gray-700 w-fit p-2 rounded text-white' }>{ article.source.name }</p>
+      <h5 className={ 'p-2 text-sm italic' }>{ article.author !== "" ? article.author : '' }</h5>
+    </div>
+  </div>
+);
+
+export default NewsCard;
